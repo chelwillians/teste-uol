2537689487aaceb907aa99f5e0fae5803af60f5f
chelwillians <michel_wrq@hotmail.com>
2019-04-27 17:19:45 -0300

    Criar função para filtrar por ID do usuário e Status

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,8 +2,45 @@
 let filterIcon = document.querySelector('.filter-icon');
 let filters = document.querySelector('.filters');
 
-filterIcon.onclick = function(){
+filterIcon.onclick = function () {
     this.classList.toggle('active');
     filters.classList.toggle('active');
 }
 
+// Create filters
+let inputUserId = document.querySelector('input#userid');
+let inputStatus = document.querySelector('input#status');
+let inputs = document.querySelectorAll('.filter input');
+let rowIDUsers = document.querySelectorAll('tr td:nth-child(2)');
+let rowStatus = document.querySelectorAll('tr td:nth-child(4)');
+
+for (let input of inputs) {
+    input.onkeyup = function () {
+        let input_IDUsers = inputUserId.value;
+        let input_Status = inputStatus.value;
+    
+        if (input_IDUsers != "" || input_Status != "") {
+            for (let i = 0; i < rowIDUsers.length; i++) {
+                rowIDUsers[i].parentElement.style.display = "table-row"; // Reset rows
+
+                if (input_IDUsers == rowIDUsers[i].textContent  && input_Status == rowStatus[i].textContent) {
+                    rowIDUsers[i].parentElement.style.display = "table-row";
+                }
+                else if (input_IDUsers == ""  && input_Status == rowStatus[i].textContent){
+                    rowIDUsers[i].parentElement.style.display = "table-row";
+                }
+                else if (input_IDUsers == rowIDUsers[i].textContent  && input_Status == ""){
+                    rowIDUsers[i].parentElement.style.display = "table-row";
+                }
+                else {
+                    rowIDUsers[i].parentElement.style.display = "none";
+                }
+            }
+    
+        } else {
+            for (let j = 0; j < rowIDUsers.length; j++) {
+                rowIDUsers[j].parentElement.style.display = "table-row";
+            }
+        }
+    }
+}
